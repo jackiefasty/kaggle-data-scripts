8265ac6e84ac6a8b1805aad156603ded95fae0e2
Borja Javierre <borja.general@gmail.com>
2023-10-21 01:53:57 +0200

    add comment to file

--- underfittingAndOverfitting.py
+++ underfittingAndOverfitting.py
@@ -39,3 +39,5 @@ def getMae(input_max_leaf_nodes, train_X, val_X, train_y, val_y):
 
 #compare different tree sizes
 candidate_max_leaf_nodes = [5, 25, 50, 100, 250, 500]
+
+#loop to find the ideal tree size from candidate_max_leaf_nodes
