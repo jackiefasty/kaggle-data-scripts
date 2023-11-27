d8f0ee6324b2ae0539b00f55d02aae3a844e9cec
Borja Javierre <borja.general@gmail.com>
2023-11-27 23:34:48 +0100

    define RF model and calculate MAE from it

--- machine-learning-competition.py
+++ machine-learning-competition.py
@@ -29,3 +29,11 @@ X.head()
 
 # Split into validation and training data
 train_X, val_X, train_y, val_y = train_test_split(X,y, random_state=1)
+
+# Define a random forest model
+rf_model = RandomForestRegressor(random_state=1)
+rf_model.fit(train_X, train_y)
+rf_val_predictions = rf_model.predict(val_X)
+rf_val_mae = mean_absolute_error(rf_val_predictions, val_y)
+
+print("Validation MAE for Random Forest Model: {:,.0f}".format(rf_val_mae))
