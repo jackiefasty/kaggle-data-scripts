71682fdb43525cd3da88c0fc5e1a2595daeecb1f
Borja Javierre <borja.general@gmail.com>
2023-11-27 23:28:02 +0100

    machine-learning-competition.py

--- machine-learning-competition.py
+++ machine-learning-competition.py
@@ -23,3 +23,15 @@ y = home_data.SalePrice
 # Creta X (After completing the excercise, return to modify this line)
 features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
 
+# Select the columns corresponding to features, and preview the data
+X = home_data[features]
+X.head()
+
+# Split into validation and training data
+train_X, val_X, train_y, val_y = train_test_split(X,y, random_state=1)
+
+# Define a random forest model
+rf_model = RandomForestRegressor(random_state=1)
+rf_model.fit(train_X, train_y)
+rf_val_predictions = rf_model.predict(val_X)
+rf_val_mae = mean_absolute_error(rf_val_predictions, val_y)
