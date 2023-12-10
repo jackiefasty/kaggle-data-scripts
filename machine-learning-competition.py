92ec5e2d7a5bfc2f81dfd12d237fb74d31213276
Borja Javierre <borja.general@gmail.com>
2023-12-11 00:21:44 +0100

    fix ML Competition script

--- machine-learning-competition.py
+++ machine-learning-competition.py
@@ -1,11 +1,12 @@
 # Set up code checking
 from learntools.core import binder
+import os
 binder.bind(globals())
 from learntools.machine_learning.ex7 import *
 
 # Set up filepaths
 import os
-if not os.path.exists("../input/train.csv"):
+if not os.path.exists("../datasets/train.csv"):
     os.symlink("../input/home-data-for-ml-course/train.csv", "../input/train.csv")  
     os.symlink("../input/home-data-for-ml-course/test.csv", "../input/test.csv")
 
