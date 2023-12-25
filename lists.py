10f726b0ac66b91f113a5af699a57089eb783ca5
Borja Javierre <borja.general@gmail.com>
2023-12-25 18:34:02 +0100

    add list script

---
+++ lists.py
@@ -0,0 +1,17 @@
+# List of integers
+primes = [2, 3, 5, 7]
+
+# List of strings
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+
+# Two lists of lists
+hands = [
+    ['J', 'Q', 'K'],
+    ['2', '2', '2'],
+    ['6', 'A', 'K'], # The comma after the last element is optional)
+]
+
+hands = [['J', 'Q', 'K'], ['2', '2', '2'], ['6', 'A', 'K']]
+
+
+
