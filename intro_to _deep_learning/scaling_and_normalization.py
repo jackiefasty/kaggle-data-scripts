e7c03d3e0a2aa4160b7e95437270847f78fb9d3e
Borja Javierre <borja.general@gmail.com>
2024-10-07 16:09:13 +0200

    add scaling and normalization script

--- intro_to _deep_learning/scaling_and_normalization.py
+++ intro_to _deep_learning/scaling_and_normalization.py
@@ -18,3 +18,11 @@ ax[0].set_title("Original Data")
 sns.histplot(scaled_data, ax=ax[1], kde=True, legend=False)
 ax[1].set_title("Scaled_data")
 plt.show()
+
+# Normalize the exponential data with boxcox
+normalized_data = stats.boxcos(original_data)
+fig, ax = plt.subplots(1,2,figsize=(15, 3))
+sns.histplot(original_data, ax=ax[0], kde=True, legend=False)
+ax[0].set_title("Original Data")
+sns.histplot(original_data, ax=ax[1], kde=True, legend=False)
+plt.show()
