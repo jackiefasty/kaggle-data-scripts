559707736c0e3f36051587b3acc9f3d7823928d4
Borja Javierre <borja.general@gmail.com>
2024-10-25 15:06:25 +0200

    add target encoding script

---
+++ feature_engineering/target_encoding.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+# Read the a CSV file
+autos = pd.read_csv("../input/fe-course-data/autos.csv")
+
+# Target encoding
+autos["make_encoded"] = autos.groupby("make")["price"].transform("mean")
+autos[["make", "price", "make_encoded"]].head(10)
+
+# Example - MovieLens1M¶
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import warnings
+
+plt.style.use("seaborn-whitegrid")
+plt.rc("figure", autolayout=True)
+plt.rc(
+    "axes",
+    labelweight="bold",
+    labelsize="large",
+    titleweight="bold",
+    titlesize=14,
+    titlepad=10,
+)
+warnings.filterwarnings('ignore')
+
+
+df = pd.read_csv("../input/fe-course-data/movielens1m.csv")
+df = df.astype(np.uint8, errors='ignore') # reduce memory footprint
+print("Number of Unique Zipcodes: {}".format(df["Zipcode"].nunique()))
+
+X = df.copy() # Create copy of a dataframe
+y = X.pop('Rating')
+
+X_encode = X.sample(frac=0.25)
+y_encode = y[X_encode.index]
+X_pretrain = X.drop(X_encode.index)
+y_train = y[X_pretrain.index]
+
+# Implement the m-estimate encoder
+from category_encoders import MEstimateEncoder
+
+# Create the encoder instance. Choose m to control noise.
+encoder = MEstimateEncoder(cols=["Zipcode"], m=5.0)
+
+# Fit the encoder on the encoding split.
+encoder.fit(X_encode, y_encode)
+
+# Encode the Zipcode column to create the final training data
+X_train = encoder.transform(X_pretrain)
+
+# Compare the encoded values to the target
+plt.figure(dpi=90)
+ax = sns.distplot(y, kde=False, norm_hist=True)
+ax = sns.kdeplot(X_train.Zipcode, color='r', ax=ax)
+ax.set_xlabel("Rating")
+ax.legend(labels=['Zipcode', 'Rating']); 
